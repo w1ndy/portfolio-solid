39659cf60c377349862e2b64b1db73add136227b
Di Weng <diweng@microsoft.com>
2023-03-06 20:35:58 +0800

    update

--- src/components/Preamble.tsx
+++ src/components/Preamble.tsx
@@ -1,39 +1,43 @@
-import { Component } from 'solid-js'
+import { Component, For, Show } from 'solid-js'
+import Contact from '../assets/contact.json'
 
 export const Preamble: Component = () => {
   return (
     <div class="text-center pt-15">
-      <h1 class="text-4xl md:text-5xl font-bold pb-3">Dr. Di Weng</h1>
+      <h1 class="text-4xl md:text-5xl font-bold pb-3">
+        {Contact.name} (
+        <span class="text-3xl md:text-4xl">{Contact.name_zh}</span>)
+      </h1>
       <div class="text-sm md:text-base">
         <div>
           <a
             class="underline-transparent"
-            href="https://goo.gl/maps/jRBWm5roD5grGCNCA"
+            href={Contact.address_map}
             target="_blank"
           >
-            Microsoft Research Asia, Tower 2, No. 5 Danling Street, Haidian
-            District, Beijing, China
+            {Contact.address}
           </a>
         </div>
         <div>
-          <span class="text-gray-400">Work:</span> diweng [at] microsoft.com
-          &#9642; <span class="text-gray-400">Personal:</span> dweng.cs [at]
-          outlook.com &#9642; <span class="text-gray-400">CV:</span>{' '}
-          <a href="https://dwe.ng">https://dwe.ng</a> &#9642;{' '}
-          <span class="text-gray-400">Social:</span>{' '}
-          <a
-            rel="me"
-            href="https://social.dwe.ng/@dw_vis"
-          >
-            Mastodon
-          </a>
-          ,{' '}
-          <a
-            rel="me"
-            href="https://twitter.com/dw_vis"
-          >
-            Twitter
-          </a>
+          {Contact.email} &#9642;{' '}
+          <a href={Contact.google_scholar}>Google Scholar</a> &#9642;{' '}
+          <For each={Object.entries(Contact.social)}>
+            {(social, index) => (
+              <>
+                <a
+                  rel="me"
+                  href={social[1]}
+                >
+                  {social[0]}
+                </a>
+                <Show
+                  when={index() != Object.entries(Contact.social).length - 1}
+                >
+                  ,{' '}
+                </Show>
+              </>
+            )}
+          </For>
         </div>
       </div>
     </div>
